167e7b51558da98a4be6f6b75f082288a3c69a1e
Boris M <borismichiels@gmail.com>
2020-12-06 10:57:01 +0100

    Update SCLinksExtractor.py

--- SCLinksExtractor.py
+++ SCLinksExtractor.py
@@ -1,18 +1,14 @@
-removeString = "" # The string you want to remove from the urls (the playlist string such as "?in=AccountName/sets/PlaylistName/"). Some files refuse to download if you don't remove this.
-
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', action='store', type=str, help="The file you want to extract the links from")
-parser.add_argument('-r', '--remove', action='store', type=str, nargs='?', help="The part of the urls that you want to remove (e.g. playlist url)")
+parser.add_argument('-r', '--remove', action='store', type=str, default="", help="The part of the urls that you want to remove (e.g. playlist url)")
 parser.add_argument('-s', '--selector', action='store', type=str, default="/", help="The end of the url to select on")
 args = parser.parse_args()
 
 SCLinksFileName = args.file
+removeString = args.remove
 linkSelector = args.selector
-
-if args.remove:
-    removeString = args.remove
 
 with open(SCLinksFileName, 'r') as file:
     lines = file.readlines()
