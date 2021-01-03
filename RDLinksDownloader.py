017396c58ce0ed08b247421c9bbc358502dd1f68
Boris Michiels <borismichiels@gmail.com>
2021-01-03 22:06:29 +0100

    Remove windows version and rename linux version

--- RDLinksDownloader.py
+++ RDLinksDownloader.py
@@ -1,49 +1,24 @@
-dirName = 'Music'
-
-from urllib.request import urlopen, urlretrieve
-import os, argparse
+import argparse, subprocess, os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', action='store', type=str, help="The file that has the links you want to download")
-parser.add_argument('-d', '--directory', action='store', type=str, nargs='?', help="The directory you want the files to be saved in")
+parser.add_argument('-d', '--directory', action='store', type=str, nargs='?', default="Music", help="The directory you want the files to be saved in")
 args = parser.parse_args()
 
 RDLinksFileName = args.file
+dirName = dirBaseName = args.directory
 
-if args.directory:
-    dirName = args.directory
+# Create directory name
+dirCount = 0
 
-# Create music directory
-dirCount = ''
-
-if os.path.exists(dirName):
-    dirCount = 1
-while os.path.exists(dirName + str(dirCount)):
+while os.path.exists(dirName):
     dirCount += 1
-dirName = dirName + str(dirCount)
-os.mkdir(dirName)
-
+    dirName = dirBaseName + "-" + str(dirCount)
 
 # Download files
-nameCount = 0
-sName = ''
-downloadCount = 0
-
 with open(RDLinksFileName, 'r') as file:
-    lines = file.readlines()
+    links = file.readlines()
 
-for link in lines:
-    link = link.rstrip()
-    name = link.rsplit("/", 1)[-1].replace("%20", " ")
-    fileName = os.path.join(dirName, name)
-    while os.path.isfile(fileName.rsplit(".", 1)[0] + str(sName) + "." + fileName.rsplit(".", 1)[1]):
-        nameCount += 1
-        sName = "_" + str(nameCount)
-    fileName = fileName.rsplit(".", 1)[0] + str(sName) + "." + fileName.rsplit(".", 1)[1]
-    urlretrieve(link, fileName)
-    print("Downloaded: " + name)
-    downloadCount += 1
-
-print("------------------------------------")
-print("Downloaded " + str(downloadCount) + " files")
-print("------------------------------------")
+for link in links:
+    url = link.rstrip()
+    subprocess.call(["wget", url, "-P", dirName])
